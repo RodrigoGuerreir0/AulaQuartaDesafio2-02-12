0d903fd673415b432b85ea665706a8c5b02df52b
Rodrigo Guerreiro <rodrigoguerreiro217@gmail.com>
2022-12-07 09:30:48 -0300

    Add files via upload

---
+++ Desafio2 02 12/Desafio2 02 12/Desafio2 02 12/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Desafio2_02_12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botao1 = new System.Windows.Forms.Button();
+            this.link1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // botao1
+            // 
+            this.botao1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.botao1.Location = new System.Drawing.Point(48, 73);
+            this.botao1.Name = "botao1";
+            this.botao1.Size = new System.Drawing.Size(226, 78);
+            this.botao1.TabIndex = 0;
+            this.botao1.Text = "Clique Aqui!";
+            this.botao1.UseVisualStyleBackColor = false;
+            this.botao1.Click += new System.EventHandler(this.botao1_Click);
+            // 
+            // link1
+            // 
+            this.link1.AutoSize = true;
+            this.link1.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.link1.LinkColor = System.Drawing.Color.Black;
+            this.link1.Location = new System.Drawing.Point(67, 183);
+            this.link1.Name = "link1";
+            this.link1.Size = new System.Drawing.Size(71, 32);
+            this.link1.TabIndex = 1;
+            this.link1.TabStop = true;
+            this.link1.Text = "Loja";
+            this.link1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link1_LinkClicked);
+            this.link1.Click += new System.EventHandler(this.link);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(870, 477);
+            this.Controls.Add(this.link1);
+            this.Controls.Add(this.botao1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botao1;
+        private System.Windows.Forms.LinkLabel link1;
+    }
+}
+
---
+++ Desafio2 02 12/Desafio2 02 12/Desafio2 02 12/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace Desafio2_02_12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            link1.Visible= false;   
+        }
+
+        private void botao1_Click(object sender, EventArgs e)
+        {
+            link1.Visible= true;
+            
+        }
+
+        private void link(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.malvile.com");
+        }
+
+        private void link1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+    }
+}
